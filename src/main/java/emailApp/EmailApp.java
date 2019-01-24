8395f1d85ea8c166fae450434bf7112d2dc279b4
Krystian Niemczewski <krystianiemczewski@gmail.com>
2019-01-24 16:39:48 +0100

    simple EmailAdministration Application

---
+++ src/main/java/emailApp/EmailApp.java
@@ -0,0 +1,13 @@
+package emailApp;
+
+public class EmailApp {
+
+    public static void main(String[] args) {
+        Email example = new Email("Chris", "Niemczewski");
+
+        System.out.println(example.showInfo());
+
+
+
+    }
+}
---
+++ src/test/java/emailApp/EmailTest.java
@@ -0,0 +1,34 @@
+package emailApp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmailTest {
+
+    @org.junit.jupiter.api.Test
+    void setMailBoxCapicity() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setAlternateEmail() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void changePassword() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getMailBoxCapicity() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAlternateEmail() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getPassword() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void showInfo() {
+    }
+}
